a487d23b324e1f96d50e1a69a5b9d5e748f75442
Aseiide <eiseiide@gmail.com>
2021-06-29 15:32:23 +0900

    sign up画面で名前等の付加情報をつけられるようにした

---
+++ db/migrate/20210629062204_add_columns.rb
@@ -0,0 +1,8 @@
+class AddColumns < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string, null: false, null: false
+    add_column :users, :postal_code, :integer, null: false
+    add_column :users, :address, :string, null: false
+    add_column :users, :biography, :text, null: false
+  end
+end
