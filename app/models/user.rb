b8592d72bcbb7f45b0b116113d16a58baa3275ae
Aseiide <eiseiide@gmail.com>
2021-07-07 00:45:22 +0900

    ユーザー詳細ページにフォロー数とフォロワー数を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  # フォローの1対多の関係
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  # フォロワーの1対多の関係
+  has_many :passive_relationshops, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
+
+  # フォローする
+  def follow(other_user)
+    follwing << other_user
+  end
+
+  # フォロー解除
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたらtrueを返す
+  def following?
+    followings.include?(other_user)
+  end
 end
