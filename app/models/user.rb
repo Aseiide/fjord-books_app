5480179f7d3bdb8f68e609c3bea43669ec6cb9d8
Aseiide <eiseiide@gmail.com>
2021-07-07 11:04:19 +0900

    typoを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy,
-                                  inverse_of: follower
+                                  inverse_of: :follower
   # フォロワーの1対多の関係
   has_many :passive_relationshops, class_name: 'Relationship',
                                    foreign_key: 'followed_id',
                                    dependent: :destroy,
-                                   inverse_of: followed
+                                   inverse_of: :followed
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationshops, source: :follower
 
