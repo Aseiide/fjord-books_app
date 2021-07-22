adb907d2007c2cdcf1d041a50a2eec79823c26c4
Aseiide <eiseiide@gmail.com>
2021-07-22 16:18:36 +0900

    フォローに関する実装を完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,19 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :following, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?
+    following.include?(other_user)
+  end
 end
