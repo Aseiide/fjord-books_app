fe490d551fcce76ed39f155d01fedb6b88e537a5
Aseiide <eiseiide@gmail.com>
2021-07-22 15:54:47 +0900

    UserとRelationshipの関連付けを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
 end
