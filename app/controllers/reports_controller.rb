5e08085557ca10e38d6ade67a2c228fcc029e53d
Aseiide <eiseiide@gmail.com>
2021-07-25 12:05:43 +0900

    更新日順に並び替えるようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
 
   # GET /reports
   def index
-    @reports = Report.all
+    @reports = Report.all.order(updated_at: :desc).page(params[:page])
   end
 
   # GET /reports/1
