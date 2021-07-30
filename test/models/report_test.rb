512c0b0e9226275a1e1c2e8f9bf88a6075b993dd
Aseiide <eiseiide@gmail.com>
2021-07-30 15:08:45 +0900

    テストをパスするようになった

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable' do
-    user = User.create!(email: 'user@example.com', password: 'password')
-    report = Report.create!(user: user, title: '日報テスト', content: 'こんにちは')
+    user = build(:user)
+    report = create(:report, user: user)
 
     assert report.editable?(user)
   end
